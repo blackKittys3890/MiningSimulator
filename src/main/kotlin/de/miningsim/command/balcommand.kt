f92f868f0fbe8d57103be039da112539ac625f44
blackKittys3890 <ohnezahn@sudi.de>
2026-04-19 10:06:16 +0200

    Fix formatting in balance message for player command Added 26.1.1 support

--- src/main/kotlin/de/miningsim/command/balcommand.kt
+++ src/main/kotlin/de/miningsim/command/balcommand.kt
@@ -11,7 +11,7 @@ class BalCommand(private val plugin: MiningSimulator) : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player) { sender.sendMessage("Nur für Spieler."); return true }
         val data = plugin.dataManager.get(sender.uniqueId, sender.name)
-        sender.sendMessage("§6💰 Dein Kontostand: §e${"%.2f".format(data.coins)} Coins")
+        sender.sendMessage("§6 Dein Kontostand: §e${"%.2f".format(data.coins)} Coins")
         return true
     }
 }
